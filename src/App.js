66284d6a08cc62904b1f6d8c8c6018121fb31da4
VarunKrishnanV <varunkrishnan0001@gmail.com>
2021-09-17 23:28:19 +0530

    router intro, browserRouter as router, Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import React from "react";
+import { Route } from "react-router";
+
+function Homepage() {
+    return <h1>Hello</h1>
+}
+function Contact() {
+    return <h1>This is a contact page</h1>
+}
+
+
 function App() {
     return (
         <>
+            <Route path="/" exact component={Homepage}>
+
+            </Route>
+            <Route path="/contact" exact component={Contact}>
+
+            </Route>
         </>
     );
 }

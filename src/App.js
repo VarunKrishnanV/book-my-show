4848cb465656c78e92fbedc59859ac74a90507f7
VarunKrishnanV <varunkrishnan0001@gmail.com>
2021-09-21 01:17:35 +0530

    hero carousel was created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Route } from "react-router";
 import DefaultHOC from "./HOC/DefaultHOC";
 
+// importing slick carousel css
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 function Homepage() {
     return <h1>This homepage</h1>
 }
--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import HeroCarousel from '../components/HeroCarousel/HeroCarousel.Component'
 import Navbar from '../components/Navbar/Navbar.Component'
 
 function DefaultLayout(props) {
     return (
         <div>
             <Navbar />
+            <HeroCarousel />
             {props.children}
 
             {console.log(props.children)}
-            <div className="footer">footer</div>
+            <div className="footer">This is footer</div>
         </div>
     )
 }
